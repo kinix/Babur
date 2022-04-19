520f090e4daeea114483955eed442073ae99be22
Uğur Kınık <ugurkinik@gmail.com>
2022-04-19 18:04:35 +0300

    Add logs for messages

--- handler.go
+++ handler.go
@@ -11,6 +11,13 @@ type Handler interface {
 }
 
 func (babur *Bot) MessageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Log messages
+	if len(m.Message.Content) > 120 {
+		fmt.Printf("MSG: %s ...\n", m.Message.Content[:120])
+	} else {
+		fmt.Printf("MSG: %s\n", m.Message.Content)
+	}
+
 	// Ignore messages from the bot itself
 	if m.Author.ID == s.State.User.ID {
 		return
@@ -26,6 +33,9 @@ func (babur *Bot) MessageHandler(s *discordgo.Session, m *discordgo.MessageCreat
 func sendMessage(s *discordgo.Session, m *discordgo.MessageCreate, content string) {
 	// Mention the user in the first line
 	msg := fmt.Sprintf("<@%s> \n%s", m.Message.Author.ID, content)
+
+	// Log reponses
+	fmt.Printf("RESPONSE: %s\n", msg)
 
 	if _, err := s.ChannelMessageSend(m.ChannelID, msg); err != nil {
 		fmt.Println("ERROR: Send message: ", err)
